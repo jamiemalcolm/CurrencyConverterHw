00958d03f26f2b01cad706ac4dacf4e75c1d017a
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-12 17:50:35 +0000

    initial commit, starter files

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Vue({
+    el: "#app",
+    data:{
+    },
+    methods:{
+
+    },
+  })
+  
+  
+});
+
+
+
+console.log("hello World");
