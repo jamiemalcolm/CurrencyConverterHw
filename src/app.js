3872e34347b092a5653a9bfd8fda4e0dafb2854b
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-12 18:31:29 +0000

    displaying rates in drop down, linked to selected rate in data

--- src/app.js
+++ src/app.js
@@ -4,9 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
   new Vue({
     el: "#app",
     data:{
+      allRates: [],
+      selectedRate: 0,
+
+    },
+    mounted: function (){
+      this.getRates();
     },
     methods:{
-
+      
+      
+      getRates: function (){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(res => res.json())
+        .then(data => this.allRates = data.rates);
+        // console.log(this.rates);
+      }
     },
   })
   
@@ -14,5 +27,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-console.log("hello World");
