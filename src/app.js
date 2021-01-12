da35ca47c3b9ff7cb7e0eb12b3143938919a37cc
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-12 18:58:33 +0000

    mvp complete, no styling

--- src/app.js
+++ src/app.js
@@ -6,20 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
     data:{
       allRates: [],
       selectedRate: 0,
+      amountToConvert: 0,
+      convertedAmount: 0,
+      direction: ""
 
     },
     mounted: function (){
       this.getRates();
     },
     methods:{
-      
-      
       getRates: function (){
         fetch('https://api.exchangeratesapi.io/latest')
         .then(res => res.json())
         .then(data => this.allRates = data.rates);
-        // console.log(this.rates);
+      },
+      convertAmount: function (){
+        if(this.direction === "fromEuros"){
+          let unRounded = this.amountToConvert * this.selectedRate;
+          this.convertedAmount =  Math.round((unRounded + Number.EPSILON) * 100) / 100;
+      }else {
+        let unRounded = this.amountToConvert / this.selectedRate;
+        this.convertedAmount = Math.round((unRounded + Number.EPSILON) * 100) / 100;
+        
       }
+    }
     },
   })
   
